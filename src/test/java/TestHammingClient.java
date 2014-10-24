dfd185285173d9978b5f6c4fb81c91b09276d71d
Elchin Asgarli <elchin@asgarli.net>
2014-10-24 19:56:03 -0400

    fixed package statements and changed code indentation

--- src/test/java/TestHammingClient.java
+++ src/test/java/TestHammingClient.java
@@ -1,57 +1,56 @@
 // Copyright (C) 2010 by Yan Huang <yhuang@virginia.edu>
 
-package Test;
-
-import java.util.*;
-import java.math.*;
-import java.security.SecureRandom;
-
 import jargs.gnu.CmdLineParser;
 
-import Utils.*;
-import Program.*;
+import java.math.BigInteger;
+import java.security.SecureRandom;
+
+import Program.HammingDistanceClient;
+import Program.ProgClient;
+import Program.Program;
+import Utils.StopWatch;
 
 class TestHammingClient {
     static BigInteger bits;
     static int n;
-    
+
     static SecureRandom rnd = new SecureRandom();
 
     private static void printUsage() {
-	System.out.println("Usage: java TestHammingClient [{-n, --bit-length} length] [{-s, --server} servername] [{-r, --iteration} r]");
+        System.out.println("Usage: java TestHammingClient [{-n, --bit-length} length] [{-s, --server} servername] [{-r, --iteration} r]");
     }
 
     private static void process_cmdline_args(String[] args) {
-	CmdLineParser parser = new CmdLineParser();
-	CmdLineParser.Option optionServerIPname = parser.addStringOption('s', "server");
-	CmdLineParser.Option optionBitLength = parser.addIntegerOption('n', "bit-Length");
-	CmdLineParser.Option optionIterCount = parser.addIntegerOption('r', "iteration");
+        CmdLineParser parser = new CmdLineParser();
+        CmdLineParser.Option optionServerIPname = parser.addStringOption('s', "server");
+        CmdLineParser.Option optionBitLength = parser.addIntegerOption('n', "bit-Length");
+        CmdLineParser.Option optionIterCount = parser.addIntegerOption('r', "iteration");
 
-	try {
-	    parser.parse(args);
-	}
-	catch (CmdLineParser.OptionException e) {
-	    System.err.println(e.getMessage());
-	    printUsage();
-	    System.exit(2);
-	}
+        try {
+            parser.parse(args);
+        }
+        catch (CmdLineParser.OptionException e) {
+            System.err.println(e.getMessage());
+            printUsage();
+            System.exit(2);
+        }
 
-	n = ((Integer) parser.getOptionValue(optionBitLength, new Integer(100))).intValue();
-	ProgClient.serverIPname = (String) parser.getOptionValue(optionServerIPname, new String("localhost"));
-	Program.iterCount = ((Integer) parser.getOptionValue(optionIterCount, new Integer(10))).intValue();
+        n = ((Integer) parser.getOptionValue(optionBitLength, new Integer(100))).intValue();
+        ProgClient.serverIPname = (String) parser.getOptionValue(optionServerIPname, new String("localhost"));
+        Program.iterCount = ((Integer) parser.getOptionValue(optionIterCount, new Integer(10))).intValue();
     }
 
     private static void generateData() throws Exception {
-	bits = new BigInteger(n, rnd);
+        bits = new BigInteger(n, rnd);
     }
 
     public static void main(String[] args) throws Exception {
-	StopWatch.pointTimeStamp("Starting program");
-	process_cmdline_args(args);
+        StopWatch.pointTimeStamp("Starting program");
+        process_cmdline_args(args);
 
-	generateData();
+        generateData();
 
-	HammingDistanceClient hammingclient = new HammingDistanceClient(bits, n);
-	hammingclient.run();
+        HammingDistanceClient hammingclient = new HammingDistanceClient(bits, n);
+        hammingclient.run();
     }
 }
--- src/test/java/TestHammingServer.java
+++ src/test/java/TestHammingServer.java
@@ -1,54 +1,51 @@
 // Copyright (C) 2010 by Yan Huang <yhuang@virginia.edu>
 
-package Test;
-
-import java.util.*;
-import java.math.*;
-import java.security.SecureRandom;
-
 import jargs.gnu.CmdLineParser;
 
-import Utils.*;
-import Program.*;
+import java.math.BigInteger;
+import java.security.SecureRandom;
+
+import Program.HammingDistanceServer;
+import Utils.StopWatch;
 
 class TestHammingServer {
     static BigInteger bits;
     static int n;
-    
+
     static SecureRandom rnd = new SecureRandom();
 
     private static void printUsage() {
-	System.out.println("Usage: java TestHammingServer [{-n, --bit-length} length]");
+        System.out.println("Usage: java TestHammingServer [{-n, --bit-length} length]");
     }
 
     private static void process_cmdline_args(String[] args) {
-	CmdLineParser parser = new CmdLineParser();
-	CmdLineParser.Option optionBitLength = parser.addIntegerOption('n', "bit-length");
+        CmdLineParser parser = new CmdLineParser();
+        CmdLineParser.Option optionBitLength = parser.addIntegerOption('n', "bit-length");
 
-	try {
-	    parser.parse(args);
-	}
-	catch (CmdLineParser.OptionException e) {
-	    System.err.println(e.getMessage());
-	    printUsage();
-	    System.exit(2);
-	}
+        try {
+            parser.parse(args);
+        }
+        catch (CmdLineParser.OptionException e) {
+            System.err.println(e.getMessage());
+            printUsage();
+            System.exit(2);
+        }
 
-	n = ((Integer) parser.getOptionValue(optionBitLength, new Integer(100))).intValue();
+        n = ((Integer) parser.getOptionValue(optionBitLength, new Integer(100))).intValue();
     }
 
     private static void generateData() throws Exception {
-	bits = new BigInteger(n, rnd);
+        bits = new BigInteger(n, rnd);
     }
 
     public static void main(String[] args) throws Exception {
 
-	StopWatch.pointTimeStamp("Starting program");
-	process_cmdline_args(args);
+        StopWatch.pointTimeStamp("Starting program");
+        process_cmdline_args(args);
 
-	generateData();
-	    
-	HammingDistanceServer hammingserver = new HammingDistanceServer(bits, n);
-	hammingserver.run();
+        generateData();
+
+        HammingDistanceServer hammingserver = new HammingDistanceServer(bits, n);
+        hammingserver.run();
     }
 }
